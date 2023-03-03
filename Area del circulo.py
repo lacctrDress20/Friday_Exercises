3c171c29b8c98d4fc988d7ab9765f7986d8d0ca8
lacctrDress20 <1490244@senati.pe>
2023-03-03 10:28:20 -0500

    new area circulo

---
+++ Area del circulo.py
@@ -0,0 +1,11 @@
+#Desarrolle un algoritmo para hallar el area de un circulo 
+#El usuario debe ingresar el radio 
+
+# Area de un circulo 
+import math
+print("ingrese el radio del circulo:")
+r=float(input())
+a=math.pi * (r * r)
+print("El área de un círculo de radio",r,"Es: ",aa)
+
+
