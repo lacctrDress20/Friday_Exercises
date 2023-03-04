6a2e46a84dc5a411f5d9bfb0aa17d8235ad6adb4
lacctrDress20 <1490244@senati.pe>
2023-03-04 09:56:13 -0500

    ultimo arreglo

--- tarea hoy.py
+++ tarea hoy.py
@@ -1,5 +1,5 @@
 # Programar un algoritmo que muestre el primer y ultimo dato de la lista
-Saludos=("Hola", "Hi", "Hello")
+Saludos=("red","blue","yellow","green","black","brown","white")
 primer_valor=Saludos[0]
 ultimo_valor=Saludos[-1]
 print("El 1er valor es: ",primer_valor)
